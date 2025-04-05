ada32a0a7b98f7a6736a7325a0621829e57448be
Christian Koop <contact@sprax2013.de>
2025-04-05 18:39:52 +0200

    Rename project and `com.craftaro` package  to `com.songoda`

---
+++ src/main/java/com/songoda/epicfurnaces/compatibility/EpicFurnacesPermission.java
@@ -0,0 +1,11 @@
+package com.songoda.epicfurnaces.compatibility;
+
+import com.songoda.third_party.com.cryptomorin.xseries.XMaterial;
+import com.songoda.skyblock.permission.BasicPermission;
+import com.songoda.skyblock.permission.PermissionType;
+
+public class EpicFurnacesPermission extends BasicPermission {
+    public EpicFurnacesPermission() {
+        super("EpicFurnaces", XMaterial.FIRE_CHARGE, PermissionType.GENERIC);
+    }
+}
---
+++ src/main/java/com/songoda/epicfurnaces/compatibility/FabledSkyBlockLoader.java
@@ -0,0 +1,17 @@
+package com.songoda.epicfurnaces.compatibility;
+
+
+import com.songoda.skyblock.SkyBlock;
+import com.songoda.skyblock.permission.BasicPermission;
+
+public class FabledSkyBlockLoader {
+    public FabledSkyBlockLoader() {
+        SkyBlock.getInstance().getPermissionManager().registerPermission(new EpicFurnacesPermission());
+
+        try {
+            SkyBlock.getInstance().getPermissionManager().registerPermission((BasicPermission) Class.forName("com.songoda.epicfurnaces.compatibility.EpicFurnacesPermission").getDeclaredConstructor().newInstance());
+        } catch (ReflectiveOperationException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
